cd6c27cd0bbab6f7c0d30c160e476936f028b22e
Soutaro Matsumoto <matsumoto@soutaro.com>
2016-01-20 07:34:04 +0900

    Initial Commit

---
+++ Obihiro/Obihiro.h
@@ -0,0 +1,19 @@
+//
+//  Obihiro.h
+//  Obihiro
+//
+//  Created by 松本宗太郎 on 2016/01/20.
+//  Copyright © 2016年 Soutaro Matsumoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Obihiro.
+FOUNDATION_EXPORT double ObihiroVersionNumber;
+
+//! Project version string for Obihiro.
+FOUNDATION_EXPORT const unsigned char ObihiroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Obihiro/PublicHeader.h>
+
+
---
+++ ObihiroTests/ObihiroTests.m
@@ -0,0 +1,39 @@
+//
+//  ObihiroTests.m
+//  ObihiroTests
+//
+//  Created by 松本宗太郎 on 2016/01/20.
+//  Copyright © 2016年 Soutaro Matsumoto. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ObihiroTests : XCTestCase
+
+@end
+
+@implementation ObihiroTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
